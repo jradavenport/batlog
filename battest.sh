960a86abc07a1633075875f78db266c1eaf55342
Kyle Marek-Spartz <zeckalpha@gmail.com>
2013-11-05 18:13:54 -0600

    Use a different date format

--- battest.sh
+++ battest.sh
@@ -1,4 +1,4 @@
-date >> ~/batlog.dat
+date "+%Y-%m-%d %H:%M:%S" >> ~/batlog.dat
 /usr/sbin/ioreg -l | grep "\"CycleCount" >> ~/batlog.dat
 /usr/sbin/ioreg -l | grep CurrentCapacity >> ~/batlog.dat
 /usr/sbin/ioreg -l | grep ExternalConnected >> ~/batlog.dat
