db270c874ddd88b0313358fbb04a46c6ad3c449b
James Davenport <jradavenport@gmail.com>
2014-07-26 14:40:06 -0700

    add quotes

--- battest.sh
+++ battest.sh
@@ -1,3 +1,3 @@
 date >> ~/batlog.dat
-/usr/sbin/ioreg -l | grep CycleCount >> ~/batlog.dat
-/usr/sbin/ioreg -l | grep Capacity >> ~/batlog.dat
+/usr/sbin/ioreg -l | grep "CycleCount" >> ~/batlog.dat
+/usr/sbin/ioreg -l | grep "Capacity" >> ~/batlog.dat
