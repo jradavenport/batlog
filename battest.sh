70a0742853e7c038669f79d36fd83022acf444fe
Jeremy Edberg <jedberg@gmail.com>
2014-07-27 22:19:35 -0700

    Make battest.sh more efficient
    I used egrep to reduce the number of times ioreg is called, which ironically reduces battery usage.

--- battest.sh
+++ battest.sh
@@ -1,3 +1,2 @@
 date >> ~/batlog.dat
-/usr/sbin/ioreg -l | grep "CycleCount" >> ~/batlog.dat
-/usr/sbin/ioreg -l | grep "Capacity" >> ~/batlog.dat
+/usr/sbin/ioreg -l | egrep "CycleCount|Capacity" >> ~/batlog.dat
